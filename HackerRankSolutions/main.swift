b8b031af607efb24e9880e54a5c2c07a32ef93f2
Ralitsa Petrina <ralitsa.petrina@gmail.com>
2020-03-08 19:29:10 +0200

    Initial Commit

---
+++ HackerRankSolutions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRankSolutions
+//
+//  Created by Petrina on 8.03.20.
+//  Copyright © 2020 Ralitsa Petrina. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

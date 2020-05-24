c52d9f8d9fce974860898a83ba4992b068077224
ralitsapetrina <ralitsa.petrina@icloud.com>
2020-05-25 00:03:33 +0300

    Project Euler #1-Multiples of 3 and 5
    Submission 5: Updated code to do all calculations with arithmetic progression formulas and no loop, which significantly decreased runtime. However the 2 test cases which failed for time, now fail with wrong answer.

--- main.swift
+++ main.swift
@@ -8,38 +8,4 @@
 
 import Foundation
 
-func calcFivesCount(num: Int) -> Int {
-    var fivesCount: Int = ((num * 2) / 10)
-    if num.isMultiple(of: 10) {
-        fivesCount -= 1
-    }
-    return fivesCount
-}
-
-func calcThreesCount(num: Int) -> Int {
-    var threesCountRaw: Double = (Double(num) / 3)
-    if floor(threesCountRaw) == threesCountRaw {
-        threesCountRaw -= 1
-    }
-    return Int(threesCountRaw)
-}
-
-let testCasesInput = readLine()!
-
-let testCases = Int(testCasesInput)!
-var array: Array<Int> = []
-
-for _ in 1...testCases {
-
-let input = readLine()
-let num: Int? = Int(input!)
-var sum: Int = 0
-
-
-
-    }
-    
-
-
-
 
